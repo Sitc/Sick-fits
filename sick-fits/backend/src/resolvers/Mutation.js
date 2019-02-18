651f01667e92d3a219b7205c9714000569d2b803
Attila <aranyi8921@gmail.com>
2019-02-18 10:35:36 +0100

    apollo on frontend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,23 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO check if they are logged in
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+    // console.log(item);
+    return item;
+  }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog)
+  //   return newDog
+  //   console.log(args)
+  // },
+
+};
+
+module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  items: forwardTo('db'),
+  // async item(parent, args, ctx, info) {
+  //   console.log("getting items")
+  //  const items = await ctx.db.query.items();
+  //  return items;
+  // }
+
+};
 
 module.exports = Query;
